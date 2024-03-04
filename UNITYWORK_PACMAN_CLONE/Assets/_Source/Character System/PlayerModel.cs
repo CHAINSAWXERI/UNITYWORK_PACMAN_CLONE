32dffab278931c4314839377562d9804c047fede
CHAINSAWXERI <123538769+CHAINSAWXERI@users.noreply.github.com>
2024-03-04 23:27:58 +0300

    add build

--- UNITYWORK_PACMAN_CLONE/Assets/_Source/Character System/PlayerModel.cs
+++ UNITYWORK_PACMAN_CLONE/Assets/_Source/Character System/PlayerModel.cs
@@ -1,10 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
-using UnityEngine.UI;
 
 namespace CharacterSystem
 {
